fd68f04102a749c2cfa1ab0e3e28de0711f18958
drug007 <drug2004@bk.ru>
2018-09-25 21:32:54 +0300

    Update dpp related.

--- run_dpp.sh
+++ run_dpp.sh
@@ -1,1 +1,1 @@
-../../3rdparties/dpp/bin/d++ ./demo/nuklear_sdl_gl3.dpp --preprocess-only --include-path ../../3rdparties/nuklear/ --include-path ./demo/c/
+../../3rdparties/dpp/bin/d++ ./demo/source/nuklear_sdl_gl3.dpp --preprocess-only --include-path ../../3rdparties/nuklear/ --include-path ./demo/c/
