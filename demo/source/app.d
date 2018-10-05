6549ba2e7bb5a48507241dc0ed00ca3fb7e412e8
drug007 <drug2004@bk.ru>
2018-10-05 17:22:44 +0300

    Test dataset contains TaggedAlgebraic values.

--- demo/source/app.d
+++ demo/source/app.d
@@ -1,101 +1,12 @@
 module demo;
 
+import gfm.math : vec3f, vec4f;
+import taggedalgebraic : TaggedAlgebraic;
+
 import beholder.application : Application;
 import beholder.renderer : Renderer;
 import beholder.actor : Actor;
-
-import gfm.math : vec3f, vec4f;
-
-struct Vertex
-{
-    vec3f position;
-    vec4f color;
-}
-
-
-
-
-
-Vertex[][] data = [
-    [
-        Vertex(vec3f(3728.77, 329.709, 0), vec4f(1, 0, 0, 1)),
-        Vertex(vec3f(4944.02, 821.47, 0), vec4f(1, 0, 0, 1)),
-        Vertex(vec3f(6921.45, 1376.91, 0), vec4f(1, 0, 0, 1)),
-        Vertex(vec3f(9409.09, 2222.93, 0), vec4f(1, 0,0, 1)),
-        Vertex(vec3f(10665.9, 3075.69, 0), vec4f(1, 0, 0, 1)),
-        Vertex(vec3f(13598.9, 3505.53, 0), vec4f(1, 0, 0, 1)),
-        Vertex(vec3f(14822.2, 4070.14, 0), vec4f(1, 0, 0, 1)),
-        Vertex(vec3f(17315.8, 4884.46, 0), vec4f(1, 0, 0, 1)), 
-        Vertex(vec3f(18690.5, 5426.98, 0), vec4f(1, 0, 0, 1)),
-        Vertex(vec3f(20577.9, 6155.25, 0), vec4f(1, 0, 0, 1)),
-        Vertex(vec3f(23452.6, 7084, 0), vec4f(1, 0, 0, 1)),
-        Vertex(vec3f(24255.9, 7631.27,0), vec4f(1, 0, 0, 1)),
-        Vertex(vec3f(25931, 8209.82, 0), vec4f(1, 0, 0, 1)),
-        Vertex(vec3f(28623.3, 8690.79, 0), vec4f(1, 0, 0, 1)),
-    ], 
-
-
-
-
-    [
-        Vertex(vec3f(3186.35, 63.7292, 0), vec4f(1, 0, 0, 1)),
-        Vertex(vec3f(5363.81, 1155.18, 0), vec4f(1, 0, 0, 1)),
-        Vertex(vec3f(7169.63, 1527.21, 0), vec4f(1, 0, 0, 1)),
-        Vertex(vec3f(8930.38, 2090.4, 0), vec4f(1, 0, 0, 1)),
-        Vertex(vec3f(11582.8, 2778.91, 0), vec4f(1, 0, 0, 1)),
-        Vertex(vec3f(12576.4, 3420.1, 0), vec4f(1, 0, 0, 1)),
-        Vertex(vec3f(14588.1, 4082.61, 0), vec4f(1, 0, 0, 1)),
-        Vertex(vec3f(17712, 4738.55, 0), vec4f(1, 0, 0, 1)),
-        Vertex(vec3f(18530.3, 5358.36, 0), vec4f(1, 0, 0, 1)),
-        Vertex(vec3f(20291.3, 6310.44, 0), vec4f(1, 0, 0, 1)),
-        Vertex(vec3f(22164.7, 6703.36, 0), vec4f(1, 0, 0, 1)),
-        Vertex(vec3f(24219.6, 7334.34, 0), vec4f(1, 0, 0, 1)),
-        Vertex(vec3f(26691.8, 8111.35, 0), vec4f(1, 0, 0, 1)),
-        Vertex(vec3f(27933.8, 8970.2, 0), vec4f(1, 0, 0, 1)),
-    ], 
-
-
-
-
-    [
-        Vertex(vec3f(7169.63, 1527.21, 0), vec4f(0.7585, 0.223, 0.937, 1)),
-        Vertex(vec3f(8977.46, 1922.87, 0), vec4f(0.7585, 0.223, 0.937, 1)),
-        Vertex(vec3f(10843.5, 2626.37, 0), vec4f(0.7585, 0.223, 0.937, 1)),
-        Vertex(vec3f(12577.6, 3350.8, 0), vec4f(0.7585, 0.223, 0.937, 1)),
-        Vertex(vec3f(14407.5, 4031.15, 0), vec4f(0.7585, 0.223, 0.937, 1)),
-        Vertex(vec3f(16220.9, 4701.82, 0), vec4f(0.7585, 0.223, 0.937, 1)),
-        Vertex(vec3f(18071.5, 5355.53, 0), vec4f(0.7585, 0.223, 0.937, 1)),
-        Vertex(vec3f(20021.9, 5996.21, 0), vec4f(0.7585, 0.223, 0.937, 1)),
-        Vertex(vec3f(21993.7, 6664.07, 0), vec4f(0.7585, 0.223, 0.937, 1)),
-        Vertex(vec3f(23879.3, 7334.3, 0), vec4f(0.7585, 0.223, 0.937, 1)),
-        Vertex(vec3f(25747.1, 7980.57, 0), vec4f(0.7585, 0.223, 0.937, 1)),
-        Vertex(vec3f(float.nan, float.nan, float.nan), vec4f(0.7585, 0.223, 0.937, 1)),
-    ], 
-
-
-
-
-
-
-    [
-        Vertex(vec3f(3728.77, 329.709, 0), vec4f(0.7585, 0.223, 0.937, 1)),
-        Vertex(vec3f(3728.77, 329.709, 0), vec4f(0.7585, 0.223, 0.937, 1)),
-        Vertex(vec3f(4968.36, 890.916, 0), vec4f(0.7585, 0.223, 0.937, 1)),
-        Vertex(vec3f(4968.36, 890.916, 0), vec4f(0.7585, 0.223, 0.937, 1)),
-        Vertex(vec3f(6456.38, 1379.53, 0), vec4f(0.7585, 0.223, 0.937, 1)),
-        Vertex(vec3f(7870.78, 1929.72, 0), vec4f(0.7585, 0.223, 0.937, 1)),
-        Vertex(vec3f(10554.2, 2488.52, 0), vec4f(0.7585, 0.223, 0.937, 1)),
-        Vertex(vec3f(12301.4, 3156.38, 0), vec4f(0.7585, 0.223, 0.937, 1)),
-        Vertex(vec3f(14064.8, 3966.86, 0), vec4f(0.7585, 0.223, 0.937, 1)),
-        Vertex(vec3f(15821.2, 4604.34, 0), vec4f(0.7585, 0.223, 0.937, 1)),
-        Vertex(vec3f(17608.1, 5300.57, 0), vec4f(0.7585, 0.223, 0.937, 1)),
-        Vertex(vec3f(19418.1, 5960.86, 0), vec4f(0.7585, 0.223, 0.937, 1)),
-        Vertex(vec3f(21191.8, 6602.49, 0), vec4f(0.7585, 0.223, 0.937, 1)),
-        Vertex(vec3f(23128.4, 7256.2, 0), vec4f(0.7585, 0.223, 0.937, 1)),
-        Vertex(vec3f(25759.9, 7929.04, 0), vec4f(0.7585, 0.223, 0.937, 1)),
-        Vertex(vec3f(float.nan, float.nan, float.nan), vec4f(0.7585, 0.223, 0.937, 1)),
-    ]
-];
+import beholder.drawer : Drawer;
 
 struct Foo
 {
@@ -113,47 +24,106 @@ struct Bar
 	string[] string_array;
 }
 
-import beholder.drawer;
-import taggedalgebraic : TaggedAlgebraic;
+struct DataPoint
+{
+    int source;
+    int dataset;
+    vec3f position;
+    long timestamp;
+}
 
 struct Types
 {
     Foo f;
     Bar b;
-    Vertex v;
+    DataPoint dp;
 }
 
 alias Value = TaggedAlgebraic!Types;
 
+struct Vertex
+{
+    vec3f position;
+    vec4f color;
+}
+
+Value[] data;
+
+static this()
+{
+    // dmd 2.081 can't initialize data in compile time so do it in static ctor
+    data = [
+        Value(DataPoint( 1, 156, vec3f( 3728.77,  329.70, 0),  10_000_000)),
+        Value(DataPoint( 2, 314, vec3f( 3186.35, 63.7292, 0),  10_000_001)),
+        Value(DataPoint(16, 273, vec3f( 7169.63, 1527.21, 0),  10_000_002)),
+        Value(DataPoint(14, 707, vec3f( 3728.77, 329.709, 0),  10_000_003)),
+        Value(DataPoint( 1, 156, vec3f( 4944.02,  821.47, 0),  20_000_000)),
+        Value(DataPoint( 2, 314, vec3f( 5363.81, 1155.18, 0),  20_000_001)),
+        Value(DataPoint(16, 273, vec3f( 8977.46, 1922.87, 0),  20_000_002)),
+        Value(DataPoint(14, 707, vec3f( 3728.77, 329.709, 0),  20_000_003)),
+        Value(DataPoint( 1, 156, vec3f( 6921.45, 1376.91, 0),  30_000_000)),
+        Value(DataPoint( 2, 314, vec3f( 7169.63, 1527.21, 0),  30_000_001)),
+        Value(DataPoint(16, 273, vec3f(10843.50, 2626.37, 0),  30_000_002)),
+        Value(DataPoint(14, 707, vec3f( 4968.36, 890.916, 0),  30_000_003)),
+        Value(DataPoint( 1, 156, vec3f( 9409.09, 2222.93, 0),  40_000_000)),
+        Value(DataPoint( 2, 314, vec3f( 8930.38, 2090.40, 0),  40_000_001)),
+        Value(DataPoint(16, 273, vec3f(12577.60, 3350.80, 0),  40_000_002)),
+        Value(DataPoint(14, 707, vec3f( 4968.36, 890.916, 0),  40_000_003)),
+        Value(DataPoint( 1, 156, vec3f(10665.90, 3075.69, 0),  50_000_000)),
+        Value(DataPoint( 2, 314, vec3f(11582.80, 2778.91, 0),  50_000_001)),
+        Value(DataPoint(16, 273, vec3f(14407.50, 4031.15, 0),  50_000_002)),
+        Value(DataPoint(14, 707, vec3f( 6456.38, 1379.53, 0),  50_000_003)),
+        Value(DataPoint( 1, 156, vec3f(13598.90, 3505.53, 0),  60_000_000)),
+        Value(DataPoint( 2, 314, vec3f(12576.40, 3420.10, 0),  60_000_001)),
+        Value(DataPoint(16, 273, vec3f(16220.90, 4701.82, 0),  60_000_002)),
+        Value(DataPoint(14, 707, vec3f( 7870.78, 1929.72, 0),  60_000_003)),
+        Value(DataPoint( 1, 156, vec3f(14822.20, 4070.14, 0),  70_000_000)),
+        Value(DataPoint( 2, 314, vec3f(14588.10, 4082.61, 0),  70_000_001)),
+        Value(DataPoint(16, 273, vec3f(18071.50, 5355.53, 0),  70_000_002)),
+        Value(DataPoint(14, 707, vec3f(10554.20, 2488.52, 0),  70_000_003)),
+        Value(DataPoint( 1, 156, vec3f(17315.80, 4884.46, 0),  80_000_000)),
+        Value(DataPoint( 2, 314, vec3f(17712.00, 4738.55, 0),  80_000_001)),
+        Value(DataPoint(16, 273, vec3f(20021.90, 5996.21, 0),  80_000_002)),
+        Value(DataPoint(14, 707, vec3f(12301.40, 3156.38, 0),  80_000_003)),
+        Value(DataPoint( 1, 156, vec3f(18690.50, 5426.98, 0),  90_000_000)),
+        Value(DataPoint( 2, 314, vec3f(18530.30, 5358.36, 0),  90_000_001)),
+        Value(DataPoint(16, 273, vec3f(21993.70, 6664.07, 0),  90_000_002)),
+        Value(DataPoint(14, 707, vec3f(14064.80, 3966.86, 0),  90_000_003)),
+        Value(DataPoint( 1, 156, vec3f(20577.90, 6155.25, 0), 100_000_000)),
+        Value(DataPoint( 2, 314, vec3f(20291.30, 6310.44, 0), 100_000_001)),
+        Value(DataPoint(16, 273, vec3f(23879.30, 7334.30, 0), 100_000_002)),
+        Value(DataPoint(14, 707, vec3f(15821.20, 4604.34, 0), 100_000_003)),
+        Value(DataPoint( 1, 156, vec3f(23452.60, 7084.00, 0), 110_000_000)),
+        Value(DataPoint( 2, 314, vec3f(22164.70, 6703.36, 0), 110_000_001)),
+        Value(DataPoint(16, 273, vec3f(25747.10, 7980.57, 0), 110_000_002)),
+        Value(DataPoint(14, 707, vec3f(17608.10, 5300.57, 0), 110_000_003)),
+        Value(DataPoint( 1, 156, vec3f(24255.90, 7631.27, 0), 120_000_000)),
+        Value(DataPoint( 2, 314, vec3f(24219.60, 7334.34, 0), 120_000_001)),
+        Value(DataPoint(14, 707, vec3f(19418.10, 5960.86, 0), 120_000_003)),
+        Value(DataPoint( 1, 156, vec3f(25931.00, 8209.82, 0), 130_000_000)),
+        Value(DataPoint( 2, 314, vec3f(26691.80, 8111.35, 0), 130_000_001)),
+        Value(DataPoint(14, 707, vec3f(21191.80, 6602.49, 0), 130_000_003)),
+        Value(DataPoint( 1, 156, vec3f(28623.30, 8690.79, 0), 140_000_000)),
+        Value(DataPoint( 2, 314, vec3f(27933.80, 8970.20, 0), 140_000_001)),
+        Value(DataPoint(14, 707, vec3f(23128.40, 7256.20, 0), 140_000_003)),
+        Value(DataPoint(14, 707, vec3f(25759.90, 7929.04, 0), 150_000_003)),
+    ];
+}
+
 alias CoordType = float;
 enum NumberOfDimensions = 3;
 
-union Payload
+struct Payload
 {
-	long l;
-	struct
-	{
-		int source;
-		int subset;
-	}
+	long index;
+	int source;
+	int subset;
 
-	this(long l)
+	this(int source, int subset, long idx)
 	{
-		this.l = l;
-	}
-
-	this(int source, int subset)
-	{
+		this.index  = idx;
 		this.source = source;
 		this.subset = subset;
-	}
-
-	void toString(scope void delegate(const(char)[]) sink) const
-	{
-		import std.format : sformat;
-		char[512] buffer = '\0';
-		auto formatted_output = sformat(buffer[], "%s(long: %s, source: %s, subset: %s)", typeof(this).stringof, l, source, subset);
-		sink(formatted_output);
 	}
 }
 
@@ -198,8 +168,7 @@ class NuklearApplication : Application
 		bar ~= Bar();
 		bar_drawer.update(bar);
 
-        import std.range, std.array;
-        value = [ Value(Foo()), Value(b), Value(data[0][0]), Value(data[0][1]), ].repeat(10_000).join.array;
+        value = data.dup;
         value_drawer = Drawer!(typeof(value))(value);
         value_drawer.update(value);
     }
@@ -271,9 +240,9 @@ class NuklearApplication : Application
                         value[0] = Bar();
                     break;
                     case Value.Kind.b:
-                        value[0] = data[0][0];
+                        value[0] = data[0];
                     break;
-                    case Value.Kind.v:
+                    case Value.Kind.dp:
                         value[0] = Foo();
                     break;
                 }
@@ -330,6 +299,15 @@ class NuklearApplication : Application
     }
 }
 
+alias LookupTable = vec4f[int];
+
+enum LookupTable lut = [
+     1 : vec4f(1.0, 0.0, 0.0, 1.0),
+     2 : vec4f(1.0, 0.5, 0.5, 1.0),
+    14 : vec4f(0.6, 0.8, 0.3, 1.0),
+    16 : vec4f(0.7, 0.9, 1.0, 1.0),
+];
+
 int main(string[] args)
 {
     auto app = new NuklearApplication("Demo gui application", 1800, 768, Application.FullScreen.no);
@@ -341,27 +319,36 @@ int main(string[] args)
 	scope(exit) renderer.destroy();
     
 	enum displace = vec3f(0.01, 0.01, 0.01);
-	int i, j;
-    foreach(e; data)
+	size_t i;
+    int[int][int] ids;
+    foreach(p; data)
     {
-		j = 0;
-		foreach(ref p; e)
-		{
-			import std.math : isNaN;
-			if (p.position.x.isNaN)
-				continue;
-			auto payload = Payload(i, j);
-			app.index.insert((p.position-displace).ptr[0..NumberOfDimensions], (p.position+displace).ptr[0..NumberOfDimensions], payload);
-			j++;
-		}
-        auto actor = renderer.make!Actor(e);
-        auto ds = actor.dataSlice;
-        ds.kind = actor.Kind.LineStrip;
-        renderer.addDataSlice(ds);
-        ds.kind = actor.Kind.Points;
-        renderer.addDataSlice(ds);
+		import std.math : isNaN;
+        if (p.kind == Value.Kind.dp)
+        {
+            auto payload = Payload(p.source, p.dataset, i);
+            app.index.insert((p.position-displace).ptr[0..NumberOfDimensions], (p.position+displace).ptr[0..NumberOfDimensions], payload);
 
-		i++;
+            i++;
+        }
+        ids[p.source][p.dataset]++;
+    }
+
+    import std.algorithm : filter, map;
+    foreach(source_id; ids.byKey)
+    {
+        foreach(dataset_id; ids[source_id].byKey)
+        {
+            auto dataset = data
+                .filter!((ref a) { return a.source == source_id && a.dataset == dataset_id; })
+                .map!(a=>Vertex(a.position, lut[a.source]));
+            auto actor = renderer.make!Actor(dataset);
+            auto ds = actor.dataSlice;
+            ds.kind = actor.Kind.LineStrip;
+            renderer.addDataSlice(ds);
+            ds.kind = actor.Kind.Points;
+            renderer.addDataSlice(ds);
+        }
     }
 
     app.add(renderer);
