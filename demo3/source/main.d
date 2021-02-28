fcce3c727b5e00bca803a03ae3c854cce2a6705e
drug007 <drug2004@bk.ru>
2021-02-28 12:18:14 +0300

    demo3 uses Panel

--- demo3/source/main.d
+++ demo3/source/main.d
@@ -74,17 +74,20 @@ int main(string[] args)
 	glViewport(0, 0, width, height);
 
 	nk_context *ctx;
-	nk_colorf bg;
 	ctx = nk_sdl_init(_window.nativeWindow);
 
 	nk_font_atlas *atlas;
 	nk_sdl_font_stash_begin(&atlas);
 	nk_sdl_font_stash_end();
 
+	nk_colorf bg;
 	bg.r = 0.10f, bg.g = 0.18f, bg.b = 0.24f, bg.a = 1.0f;
-	enum {EASY, HARD}
-	int op = EASY;
-	int property = 20;
+
+	import beholder.panel;
+	auto panels = [
+		new Panel("demo2", 550, 50, 230, 250, bg),
+		new Panel("demo", 50, 50, 230, 250, bg),
+	];
 
 	bool running = true;
 	while(running)
@@ -102,36 +105,8 @@ int main(string[] args)
 		}
 		nk_input_end(ctx);
 
-		if (nk_begin(ctx, "Demo", nk_rect(50, 50, 230, 250),
-					 NK_WINDOW_BORDER|NK_WINDOW_MOVABLE|NK_WINDOW_SCALABLE|
-					 NK_WINDOW_MINIMIZABLE|NK_WINDOW_TITLE))
-		{
-			nk_layout_row_static(ctx, 30, 80, 1);
-			if (nk_button_label(ctx, "button"))
-			{
-				printf("button pressed\n");
-			}
-			nk_layout_row_dynamic(ctx, 30, 2);
-			if (nk_option_label(ctx, "easy", op == EASY)) op = EASY;
-			if (nk_option_label(ctx, "hard", op == HARD)) op = HARD;
-			nk_layout_row_dynamic(ctx, 25, 1);
-			nk_property_int(ctx, "Compression:", 0, &property, 100, 10, 1);
-
-			nk_layout_row_dynamic(ctx, 20, 1);
-			nk_label(ctx, "background:", NK_TEXT_LEFT);
-			nk_layout_row_dynamic(ctx, 25, 1);
-			if (nk_combo_begin_color(ctx, nk_rgb_cf(bg), nk_vec2(nk_widget_width(ctx),400))) {
-				nk_layout_row_dynamic(ctx, 120, 1);
-				bg = nk_color_picker(ctx, bg, NK_RGBA);
-				nk_layout_row_dynamic(ctx, 25, 1);
-				bg.r = nk_propertyf(ctx, "#R:", 0, bg.r, 1.0f, 0.01f,0.005f);
-				bg.g = nk_propertyf(ctx, "#G:", 0, bg.g, 1.0f, 0.01f,0.005f);
-				bg.b = nk_propertyf(ctx, "#B:", 0, bg.b, 1.0f, 0.01f,0.005f);
-				bg.a = nk_propertyf(ctx, "#A:", 0, bg.a, 1.0f, 0.01f,0.005f);
-				nk_combo_end(ctx);
-			}
-		}
-		nk_end(ctx);
+		foreach(e; panels)
+			e.draw(ctx);
 
 		/* Draw */
 		glViewport(0, 0, _window.getWidth, _window.getHeight);
